7bdea38a587853b61abf21329ba2cbbf99db6b5f
Owen1225 <owendu001@gmail.com>
2022-12-02 20:33:45 -0800

    basic settings

--- main.py
+++ main.py
@@ -1,46 +1,87 @@
-# Create a 2D game that is open world and the player can collect different items and fight different mobs
 # Sources: 
+# KidsCanCode - Game Development with Pygame video series
 
+import pygame as pg
+import sys
+from settings import *
+from sprites import *
 
-# Libraries: 
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-import pygame, sys
+    def load_data(self):
+        pass
 
-#pygame setup
-pygame.init()
-WIDTH = 1200
-HEIGHT = 700
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255,255,0)
-ORANGE = (255, 98, 0)
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
-#load images
-House1 = pygame.image.load('C:/Users/Owen.Du25/Downloads/Background/House1.jpg').convert_alpha()
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
 
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
 
-#create function for drawing background
-def draw_bg():
-    screen.blit(House1, (0,0))
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
 
+    def show_start_screen(self):
+        pass
 
-while True: 
+    def show_go_screen(self):
+        pass
 
-    draw_bg()
-
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-
-    pygame.display.update()
-    clock.tick(60)
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
